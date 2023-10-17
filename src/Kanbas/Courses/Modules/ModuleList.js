04aaac723890098ee405d61a2a4a1206ff085268
aarohinadkarni <arunadkarni@gmail.com>
2023-10-17 19:10:52 -0400

    almost done modules & home

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -2,22 +2,38 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import '../../index.css'
+import { FaEllipsisV, FaPlus, FaCheckCircle } from "react-icons/fa";
+
 
 
 function ModuleList() {
   const { courseId } = useParams();
   const modules = db.modules;
   return (
-    <div class="module-content">
-      <ul className="list-group list-group-item-secondary">
+    <div className="module-content">
+      <ul className="list-group">
         {
         modules
           .filter((module) => module.course === courseId)
           .map((module, index) => (
-            <li key={index} className="list-group-item">
-              <h3>{module.name}</h3>
-              <p>{module.description}</p>
+            <>
+            <li key={index} className="list-group-item list-group-item-secondary" style={{paddingLeft: 0, paddingBottom: 0, paddingRight: 0}}>
+              <div style={{paddingLeft: 10}}>{module.name}</div>
+              <div className="d-flex justify-content-end align-items-center">
+                <FaEllipsisV  className="me-3" style={{color:"grey"}}/>
+                <FaPlus className="me-3" style={{color:"grey"}}/>
+                <FaCheckCircle className="me-3" style={{color:"green"}}/>
+              </div>
+              <div className = "list-group">
+                <li key={index} className="list-group-item green-list" >
+                  {module.description}
+                  <FaEllipsisV className="float-end" style={{color:"grey"}}/>
+                  <FaCheckCircle className="float-end" style={{color:"green"}}/>
+                 </li>
+              </div>
             </li>
+            <br/>
+            </>
         ))
         }
       </ul>
