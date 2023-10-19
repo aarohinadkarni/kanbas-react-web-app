831d621746954dbf350f74b9a28cca6554ed00e1
aarohinadkarni <arunadkarni@gmail.com>
2023-10-19 02:40:12 -0400

    assignment edits and grades

--- src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import db from "../../../Database";
+import { FaEllipsisV, FaCheckCircle } from "react-icons/fa";
+import '../index.css'
+import '../../../index.css'
 
 
 function AssignmentEditor() {
@@ -16,17 +19,153 @@ function AssignmentEditor() {
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
   };
   return (
-    <div>
-      <h2>Assignment Name</h2>
-      <input value={assignment.title}
-             className="form-control mb-2" />
-      <Link to={`/Kanbas/Courses/${courseId}/Assignments`}
-            className="btn btn-danger">
-        Cancel
-      </Link>
-      <button onClick={handleSave} className="btn btn-success me-2">
-        Save
-      </button>
+    <div className="wd-home-flex-column-container flex-grow-1 assignment-options">
+      <div className="d-flex justify-content-end course-buttons published">
+            <FaCheckCircle/>
+            <b>Published</b>
+        <span>
+        <label for="ellipsis-button"></label>
+        <button type="submit" className="btn btn-secondary btn-sm regular py-2" id="ellipsis-button">
+        <FaEllipsisV/>
+        </button>
+        </span>
+      </div>
+      <hr/>
+      <label for="assignment-name">Assignment Name</label><br/>
+      <input id="assignment-name" className="form-control" value={assignment.title} /><br />
+      <label for="assignment-description-textarea"></label><br />
+      <textarea className="form-control" cols="120" rows="2" id="assignment-description-textarea">This is the assignment description.
+      </textarea><br /><br />
+      <div className="assignment-inputs">
+        <div className="row edit">
+            <div className="col-md-3">
+                <label for="points" class="float-end">Points</label>
+            </div>
+            <div className="col-md-9">
+                <input
+                        type="text"
+                        id="points"
+                        value="100"
+                        class="form-control"
+                />
+            </div>
+        </div>
+        <div className="row edit">
+            <div className="col-md-3">
+                <label for="assignment-group" class="float-end">Assignment Group</label>
+            </div>
+            <div className="col-md-9 mb-3">
+                <select id="assignment-group" class="form-select">
+                    <option>ASSIGNMENTS</option>
+                </select>
+            </div>
+        </div>
+        <div className="row edit">
+            <div className="col-md-3">
+                <label for="display-grade" class="float-end">Display Grade as</label>
+            </div>
+            <div className="col-md-9">
+                <select className="form-select" id="display-grade">
+                    <option>Percentage</option>
+                </select>
+            </div>
+        </div>
+        <div className="row edit">
+            <div className="col-md-3">
+            </div>
+            <div class="col-md-9">
+                <input className="form-check-input" type="checkbox" value="Not counted towards final grade" name="count-towards-final-grades" id="chkbox-not-counter"></input>
+                <label className="form-check-label" for="chkbox-not-counter" style={{marginLeft: 5}}>
+                    Do not count this assignment towards final grade
+                </label>
+            </div>
+        </div>
+        <div className="row edit">
+            <div className="col-md-3">
+                <label for="submission-type" class="float-end">Submission Type</label>
+            </div>
+            <div className="col-md-9">
+                <div className="border rounded edit">
+                    <select id="submission-type" class="form-select" style={{width:"40%"}}>
+                        <option>Online</option>
+                    </select><br />
+                    <label><b>Online Entry Options</b></label>
+                    <br />
+                    <div class="form-check">
+                        <input className="form-check-input" type="checkbox" value="Text Entry" name="check-online-entry-options" id="chkbox-text-entry"></input>
+                        <label className="form-check-label" for="chkbox-text-entry">
+                            Text Entry
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value=""Website URL name="check-online-entry-options" id="chkbox-website-url"></input>
+                        <label className="form-check-label" for="chkbox-website-url">
+                            Website URL
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="Media Recordings" name="check-online-entry-options" id="chkbox-media-recordings"></input>
+                        <label className="form-check-label" for="chkbox-media-recordings">
+                            Media Recordings
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="Student Annotation" name="check-online-entry-options" id="chkbox-student-annotation"></input>
+                        <label className="form-check-label" for="chkbox-student-annotation">
+                            Student Annotation
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="File Uploads" name="check-online-entry-options" id="chkbox-file-uploads"></input>
+                        <label className="form-check-label" for="chkbox-file-uploads">
+                            File Uploads
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
+          <div className="row" style={{marginTop: 30}}>
+        <div className="col-md-3">
+            <div className="float-end">Assign</div>
+        </div>
+        <div className="col-md-9">
+            <div className="border rounded edit">
+                <label for="assign-to"><b>Assign to</b></label>
+                <input className="form-control" id="assign-to" value="Everyone" /><br/>
+                <label for="due-by"><b>Due</b></label>
+                <input className="form-control" type="date" id="due-by" value="2023-09-18" /><br/>
+                <div className="row override">
+                    <div className="col">
+                        <label for="available-from"><b>Available from</b></label>
+                        <input className="form-control" type="date" id="available-from" value="2023-09-06" />
+                    </div>
+                    <div className="col">
+                        <label for="until"><b>Until</b></label>
+                        <input className="form-control"  type="date" id="until" />
+                    </div>
+                </div>
+        </div>
+    </div>
+        </div>
+        </div>
+      </div>
+      <hr/>
+      <div className="form-check" style={{ display: 'inline', marginRight: '10px' }}>
+    <input className="form-check-input" type="checkbox" value="Not counted towards final grade" name="notify-content-changed" id="chkbox-content-changed"></input>
+            <label className="form-check-label" for="chkbox-content-changed" style={{marginLeft: 5}}>
+                Notify users that this content has changed
+            </label>
+    
+    <form id="save-button" action="index.html" style={{display: "inline"}}>
+        <label for="save-button"></label>
+        <button onClick={handleSave} className="btn btn-danger btn-sm float-end" id="save-button">Save</button>
+    </form>        
+    <form id="cancel-button" action="index.html" style={{display: "inline"}}>
+      <Link to={`/Kanbas/Courses/${courseId}/Assignments`} className="btn btn-secondary btn-sm regular float-end" id="cancel-button">Cancel</Link>
+        <label for="cancel-button"></label>
+    </form>
+    </div>
     </div>
   );
 }
--- src/Kanbas/Courses/Grades/index.js
+++ src/Kanbas/Courses/Grades/index.js
@@ -1,28 +1,66 @@
 import db from "../../Database";
 import { useParams } from "react-router-dom";
+import '../../index.css'
+import { FaFileExport, FaFileImport, FaCog, FaFilter } from "react-icons/fa";
+
 function Grades() {
   const { courseId } = useParams();
   const assignments = db.assignments.filter((assignment) => assignment.course === courseId);
   const enrollments = db.enrollments.filter((enrollment) => enrollment.course === courseId);
   return (
-    <div>
-      <h1>Grades</h1>
+    <div className="flex-column d-flex flex-grow-1" style={{width:1000}}>
+      <div>
+          <div className="d-flex justify-content-end grades-buttons">
+              <label for="import-button"></label>
+              <button type="submit" className="btn btn-secondary btn-sm regular" id="import-button">
+                  <FaFileImport/>
+                  Import</button>
+              <div className="dropdown">
+                  <button className="btn btn-light dropdown-toggle regular" type="button" data-bs-toggle="dropdown"aria-expanded="false">
+                      <FaFileExport/>
+                      Export
+                  </button>
+              </div>
+              <label for="configure-button"></label>
+              <button type="submit" className="btn btn-secondary btn-sm regular" id="configure-button"><FaCog/></button>
+          </div>
+          <br/>
+          <div>
+            <div className="row">
+                <div className="col">
+                    <label for="student-names" className="form-label"><b>Student Names</b></label><br/>
+                    <input className="form-control" id="student-names" placeholder="Search Students" />
+                </div>
+                <div className="col">
+                    <label for="assignment-names" claclassNamess="form-label"><b>Assignment Names</b></label><br/>
+                    <input className="form-control" id="assignment-names" placeholder="Search Assignments" />
+                </div>
+            </div>
+        </div>
+        <div className="grades-buttons">
+            <label for="apply-filters-button"></label>
+            <button type="submit" className="btn btn-secondary btn-md regular" id="apply-filters-button"><FaFilter/>Apply Filters</button>
+        </div>
+        <br/>
+      </div>
       <div className="table-responsive">
-        <table className="table">
+        <table className="table table-striped table-bordered heading" width="100%">
           <thead>
-            <th>Student Name</th>
-            {assignments.map((assignment) => (<th>{assignment.title}</th>))}
+          <tr>
+            <th style={{textAlign: "left"}}>Student Name</th>
+            {assignments.map((assignment) => (<th className="not-bold">{assignment.title}<br/><font size="2">Out of 100</font></th>))}
+            </tr>
           </thead>
           <tbody>
             {enrollments.map((enrollment) => {
               const user = db.users.find((user) => user._id === enrollment.user);
               return (
                 <tr>
-                   <td>{user.firstName} {user.lastName}</td>
+                   <td className="text-center align-middle">{user.firstName} {user.lastName}</td>
                    {assignments.map((assignment) => {
                      const grade = db.grades.find(
                        (grade) => grade.student === enrollment.user && grade.assignment === assignment._id);
-                       return (<td>{grade?.grade || ""}</td>);})}
+                       return (<td className="text-center align-middle">{grade?.grade || ""}</td>);})}
                 </tr>);
             })}
        </tbody></table>
