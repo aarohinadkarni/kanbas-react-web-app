2060a860460f6d6b0941dc977409fa345b88d507
aarohinadkarni <arunadkarni@gmail.com>
2023-11-01 11:19:40 -0400

    modules & redux

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import '../../index.css'
 import { FaEllipsisV, FaPlus, FaCheckCircle } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
 
 
 
 function ModuleList() {
   const { courseId } = useParams();
-  const modules = db.modules;
+  const modules = useSelector((state) => state.modulesReducer.modules);
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
   return (
     <div className="module-content">
+      <div class="row" style={{ marginLeft:20}}>
+        <div class="col-auto">
+          <input value={module.name}
+            onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}/>
+        </div>
+        <div class="col-auto">
+          <textarea value={module.description}
+            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))}/>
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-secondary btn-sm regular" onClick={() => dispatch(addModule({ ...module, course: courseId }))}>Add</button>
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-secondary btn-sm regular" onClick={() => dispatch(updateModule(module))}>Update</button>
+        </div>
+      </div>
       <ul className="list-group">
         {
         modules
@@ -29,6 +54,8 @@ function ModuleList() {
                   {module.description}
                   <FaEllipsisV className="float-end" style={{color:"grey"}}/>
                   <FaCheckCircle className="float-end" style={{color:"green"}}/>
+                  <button class = "btn btn-secondary btn-sm regular float-end" onClick={() => dispatch(setModule(module))} style={{marginRight:10}}> Edit</button>
+                  <button class = "btn btn-secondary btn-sm regular float-end" onClick={() => dispatch(deleteModule(module._id))} style={{marginRight:10}}> Delete</button>
                  </li>
               </div>
             </li>
